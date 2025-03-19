ad33a3c5f7de030e87ee2a290886cd6ab71d4d6c
David Sanmartim <davidsanm@gmail.com>
2025-03-19 12:38:21 -0300

    Add ignore property to park_dome.py and unpark_dome.py scripts. Unit tests also added.

--- tests/test_maintel_unpark_dome.py
+++ tests/test_maintel_unpark_dome.py
@@ -19,10 +19,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+import contextlib
 import unittest
 
 from lsst.ts import standardscripts
 from lsst.ts.maintel.standardscripts.mtdome import UnparkDome
+from lsst.ts.observatory.control.maintel.mtcs import MTCS, MTCSUsages
 
 
 class TestUnparkDome(
@@ -30,4 +32,42 @@ class TestUnparkDome(
 ):
     async def basic_make_script(self, index):
         self.script = UnparkDome(index=index)
-        return self.script
+        return (self.script,)
+
+    @contextlib.asynccontextmanager
+    async def make_dry_script(self):
+        async with self.make_script():
+            self.script.mtcs = MTCS(
+                domain=self.script.domain,
+                intended_usage=MTCSUsages.DryTest,
+                log=self.script.log,
+            )
+            self.script.mtcs.assert_all_enabled = unittest.mock.AsyncMock()
+            self.script.mtcs.unpark_dome = unittest.mock.AsyncMock()
+            yield
+
+    async def test_config(self):
+        async with self.make_dry_script():
+            await self.configure_script()
+
+    async def test_configure_ignore(self):
+        async with self.make_dry_script():
+            components = ["mtmount"]
+            await self.configure_script(ignore=components)
+
+            assert self.script.mtcs.check.mtmount is False
+
+    async def test_configure_ignore_not_csc_component(self):
+        async with self.make_dry_script():
+            components = ["not_csc_comp", "mtmount"]
+            await self.configure_script(ignore=components)
+
+            assert hasattr(self.script.mtcs, "not_csc_comp") is False
+            assert self.script.mtcs.check.mtmount is False
+
+    async def test_run(self):
+        async with self.make_dry_script():
+            await self.configure_script(ignore=["mtmount"])
+            await self.run_script()
+            self.script.mtcs.assert_all_enabled.assert_awaited_once()
+            self.script.mtcs.unpark_dome.assert_awaited_once()
