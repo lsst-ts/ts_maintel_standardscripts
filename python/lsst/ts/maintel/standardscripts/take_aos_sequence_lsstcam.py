059868d79ed89807a7d1db63dff27af0107029c3
gmegh <guillem.megias.homar@gmail.com>
2025-04-14 14:28:09 -0700

    Abstract oods in take_aos_sequence

--- python/lsst/ts/maintel/standardscripts/take_aos_sequence_lsstcam.py
+++ python/lsst/ts/maintel/standardscripts/take_aos_sequence_lsstcam.py
@@ -50,6 +50,10 @@ class TakeAOSSequenceLSSTCam(BaseTakeAOSSequence):
     def camera(self):
         return self._camera
 
+    @property
+    def oods(self):
+        return self._camera.rem.mtoods
+
     async def configure_camera(self) -> None:
         """Handle creating the camera object and waiting remote to start."""
         if self._camera is None:
