e06aa71675a7b2ff8956d664ef58fbfaffdde1a4
Tiago Ribeiro <tiago.ribeiro@noirlab.edu>
2025-12-22 11:38:44 -0700

    In ``track_target_and_take_image_lsstcam.py``, add a new feature that allows the script to run a configurable number of additional exposures every time the filter changes.
    These exposures are intended as a closed loop sequence to align the optics. The script will ensure that AOS closed loop is running and in the correct state prior to taking exposures. It will then wait for the corrections for the additional exposures to be published before continuing.

--- python/lsst/ts/maintel/standardscripts/track_target_and_take_image_lsstcam.py
+++ python/lsst/ts/maintel/standardscripts/track_target_and_take_image_lsstcam.py
@@ -57,7 +57,7 @@ class TrackTargetAndTakeImageLSSTCam(BaseTrackTargetAndTakeImage):
 
         mtcs_usage, lsstcam_usage = (
             (
-                MTCSUsages.Slew | MTCSUsages.StateTransition,
+                MTCSUsages.Slew | MTCSUsages.StateTransition | MTCSUsages.AOS,
                 LSSTCamUsages.TakeImageFull | LSSTCamUsages.StateTransition,
             )
             if add_remotes
@@ -102,6 +102,25 @@ class TrackTargetAndTakeImageLSSTCam(BaseTrackTargetAndTakeImage):
             "description": "Integration time for the guider ROI in milliseconds.",
             "default": 200,
             "minimum": 1,
+        }
+        schema_dict["properties"]["run_aos_closed_loop_on_filter_change"] = {
+            "type": "boolean",
+            "description": "Run AOS closed loop when there is a filter change?",
+            "default": False,
+        }
+        schema_dict["properties"]["aos_closed_loop_settings"] = {
+            "type": "object",
+            "additionalProperties": False,
+            "description": "Configuration for the aos closed loop.",
+            "default": {
+                "n_iter": 1,
+            },
+            "properties": {
+                "n_iter": {
+                    "type": "integer",
+                    "description": "Number of iterations to run.",
+                },
+            },
         }
 
         return schema_dict
@@ -182,7 +201,8 @@ class TrackTargetAndTakeImageLSSTCam(BaseTrackTargetAndTakeImage):
 
         self.tracking_started = True
 
-        if current_filter != self.config.band_filter:
+        filter_change_required = current_filter != self.config.band_filter
+        if filter_change_required:
             self.log.debug(
                 f"Filter change required: {current_filter} -> {self.config.band_filter}"
             )
@@ -201,6 +221,12 @@ class TrackTargetAndTakeImageLSSTCam(BaseTrackTargetAndTakeImage):
             az_wrap_strategy=self.config.az_wrap_strategy,
             time_on_target=self.get_estimated_time_on_target(),
         )
+
+        if filter_change_required and self.config.run_aos_closed_loop_on_filter_change:
+            self.log.info("Filter changed, running a close loop sequence.")
+            await self._run_close_loop()
+        elif filter_change_required:
+            self.log.info("Filter changed but not running close loop sequence.")
 
     async def _handle_slew_and_change_filter(self):
         """Handle slewing and changing filter at the same time.
@@ -267,7 +293,9 @@ class TrackTargetAndTakeImageLSSTCam(BaseTrackTargetAndTakeImage):
 
     async def wait_mtaos_idle(self):
         self.mtcs.rem.mtaos.evt_closedLoopState.flush()
-        mtaos_closed_loop_state = await self.mtcs.rem.mtaos.evt_closedLoopState.aget()
+        mtaos_closed_loop_state = await self.mtcs.rem.mtaos.evt_closedLoopState.aget(
+            timeout=self.mtcs.long_timeout
+        )
         self.log.info(
             f"MTAOS closed loop state: {ClosedLoopState(mtaos_closed_loop_state.state).name}."
         )
@@ -292,6 +320,114 @@ class TrackTargetAndTakeImageLSSTCam(BaseTrackTargetAndTakeImage):
                     "Last known state: {ClosedLoopState(mtaos_closed_loop_state.state).name}."
                 )
                 return
+
+    async def _wait_mtaos_ready_for_closed_loop(self):
+        self.mtcs.rem.mtaos.evt_closedLoopState.flush()
+        mtaos_closed_loop_state = await self.mtcs.rem.mtaos.evt_closedLoopState.aget(
+            timeout=self.mtcs.long_timeout
+        )
+        while mtaos_closed_loop_state.state not in {
+            ClosedLoopState.WAITING_IMAGE,
+        }:
+            try:
+                mtaos_closed_loop_state = (
+                    await self.mtcs.rem.mtaos.evt_closedLoopState.next(
+                        flush=False, timeout=self.mtcs.long_long_timeout
+                    )
+                )
+                self.log.info(
+                    f"MTAOS closed loop state: {ClosedLoopState(mtaos_closed_loop_state.state).name}."
+                )
+            except asyncio.TimeoutError as e:
+                raise RuntimeError(
+                    "Waiting for the MTAOS closed loop state to go to WAITING_IMAGE took too long.\n\n"
+                    "We need to wait for the closed loop to be in this state so we can run close "
+                    "loop sequence. This might indicate there is an issue with the Rapid Analisys "
+                    "backend or something alike delaying the image processing.\n"
+                    "You might want to reach out for support."
+                ) from e
+
+    async def _run_close_loop(self):
+        """Run a close loop sequence."""
+
+        self.mtcs.rem.mtaos.evt_closedLoopState.flush()
+        mtaos_closed_loop_state = await self.mtcs.rem.mtaos.evt_closedLoopState.aget(
+            timeout=self.mtcs.long_timeout
+        )
+        if mtaos_closed_loop_state.state == ClosedLoopState.IDLE:
+            raise RuntimeError(
+                "Current MTAOS closed loop state is IDLE. \n\n"
+                "In order to support changing filters with this script the MTAOS "
+                "close loop must be running. If you forgot to enable close loop "
+                "prior to resuming survey operations you might need to stop, "
+                "align the optics, enable close loop and resume again. "
+                "If this is intentional, request your support to disable the "
+                "filter close loop sequence feature in this script."
+            )
+        self.log.info(
+            f"MTAOS closed loop state: {ClosedLoopState(mtaos_closed_loop_state.state).name}."
+        )
+        await self._wait_mtaos_ready_for_closed_loop()
+        exptime = self.config.exp_times[0]
+        for exp in range(self.config.aos_closed_loop_settings["n_iter"]):
+            visit_ids = await self.lsstcam.take_object(
+                exptime=exptime,
+                group_id=self.group_id,
+                reason=f"{self.config.reason}_filter_change_close_loop",
+                program=self.config.program,
+                note=f"close_loop#{exp+1}",
+            )
+            wait_correction_for_visit_id_task = asyncio.create_task(
+                self.wait_correction_for_visit_id(visit_id=visit_ids[0])
+            )
+            await self.lsstcam.take_object(
+                exptime=exptime,
+                group_id=self.group_id,
+                reason=f"{self.config.reason}_filter_change_close_loop",
+                program=self.config.program,
+                note=f"extra_visit_while_waiting_for_correction#{exp+1}",
+            )
+            await wait_correction_for_visit_id_task
+
+    async def wait_correction_for_visit_id(self, visit_id: int) -> None:
+        """Wait for the AOS correction for the provided visit id.
+
+        Parameters
+        ----------
+        visit_id : `int`
+            Visit id to wait the correction for.
+        """
+        self.mtcs.rem.mtaos.evt_degreeOfFreedom.flush()
+        degree_of_freedom = await self.mtcs.rem.mtaos.evt_degreeOfFreedom.aget(
+            timeout=self.mtcs.long_timeout
+        )
+        degree_of_freedom_visit_id_index = degree_of_freedom.visitId % 100000
+        visit_id_index = visit_id % 100000
+        self.log.info(
+            f"Waiting for degree of freedom for {visit_id=} ({visit_id_index=}); "
+            f"got initial {degree_of_freedom_visit_id_index}."
+        )
+        while visit_id_index != degree_of_freedom_visit_id_index:
+            try:
+                degree_of_freedom = await self.mtcs.rem.mtaos.evt_degreeOfFreedom.next(
+                    flush=False,
+                    timeout=(
+                        self.mtcs.long_long_timeout + self.config.exp_times[0] * 2.0
+                    ),
+                )
+            except TimeoutError as e:
+                raise RuntimeError(
+                    f"Timeout waiting for AOS corrections to complete for visit {visit_id}; "
+                    f"last correction received was for {degree_of_freedom_visit_id_index}. "
+                    "This might be a sign that RA is taking too long to calculate the corrections "
+                    "or some other issue calculating the corrections themselves (e.g. too cloudy). "
+                    "Check the dataset on RubinTV and/or reach out to support for help identifying the issue."
+                ) from e
+            degree_of_freedom_visit_id_index = degree_of_freedom.visitId % 100000
+            self.log.info(
+                f"Waiting for degree of freedom for {visit_id_index=}; "
+                f"got {degree_of_freedom_visit_id_index}."
+            )
 
     async def stop_tracking(self):
         """Execute stop tracking on MTCS."""
